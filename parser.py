81c461a6c999b5402340b236576f3c3f3525abfe
shijc <shijc@umich.edu>
2017-10-13 17:19:23 -0400

    initial commit of parser

---
+++ parser.py
@@ -0,0 +1,57 @@
+import xml.etree.ElementTree as ET
+from chemkin import Reaction
+
+def string_to_dict(s):
+    # 'H:1 O2:1' -> {'H': 1.0, 'O2': 1.0}
+    ret = {}
+    for kv in s.split():
+        kv = kv.split(':')
+        ret[kv[0]] = float(kv[1])
+    return ret
+
+def read_data(filename):
+    tree = ET.parse(filename)
+    rxns = tree.getroot()
+    
+    data = {}
+    # get species list
+    phase = rxns.find('phase')
+    if phase: # check if tag 'phase' is found
+        
+        ##### DO WE NEED TO CHECK ??? #####
+        
+        data['species'] = phase.find('speciesArray').text.split()
+
+    # Reaction(reactants, products, reversible, reac_type, reac_id, coef_type):
+    data['reactions'] = {}
+    for reaction_data in rxns.findall('reactionData'):
+        reaction_id = reaction_data.get('id')        
+        reactions = []
+        for reaction in reaction_data.findall('reaction'):
+            reactants = string_to_dict(reaction.find('reactants').text)
+            products = string_to_dict(reaction.find('products').text)
+            reversible = reaction.get('reversible') != 'no'
+            reac_type = reaction.get('type')
+            reac_id = reaction.get('id')
+            coefs_block = reaction.find('rateCoeff')
+            coef = {}
+            for arr in coefs_block:
+                coef_type = arr.tag
+                params_block = coefs_block.find(coef_type)
+                for param in params_block:
+                    key = param.tag
+                    coef[key] = float(params_block.find(key).text)
+            print(reactants, products, 
+                                     reversible, reac_type, 
+                                     reac_id, coef_type, coef)
+            reactions.append(Reaction(reactants, products, 
+                                     reversible, reac_type, 
+                                     reac_id, coef_type, coef))
+            data['reactions'][reaction_id] = reactions
+    return data
+
+
+data = read_data('rxns.xml')
+
+print(data)
+print(data['reactions']['test_mechanism'][0])
