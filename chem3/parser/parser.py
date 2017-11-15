ba159f981584af316fc18c804f99d103b19af75a
shijc <shijc@umich.edu>
2017-11-15 03:23:15 -0500

    Read coefficients matrix in parser

--- chem3/parser/parser.py
+++ chem3/parser/parser.py
@@ -7,6 +7,8 @@ main directory of this library, titled 'rxns.xml'
 """
 
 import xml.etree.ElementTree as ET
+import sqlite3
+import numpy as np
 from chem3.chemkin.chemkin import *
 
 def string_to_dict(s):
@@ -31,22 +33,70 @@ def string_to_dict(s):
         ret[kv[0]] = float(kv[1])
     return ret
 
-def read_data(filename):
+
+def get_coeffs(db_name, species):
+    """reads the database and gets coefficients 
+
+    INPUTS:
+    =======
+    db_name:    str
+                Database name storing coefficients
+    species:    array of str
+                A list of species to ensure coefficient 
+                and matrix will use the same order of species array
+
+    RETURNS:
+    =======
+    low:        numpy array
+                Coefficient matrix for low temperature reactions
+    high:       numpy array
+                Coefficient matrix for high temperature reactions
+
+    EXAMPLES:
+    ========
+    >>> high = get_coeffs('nasa.sqlite', ['O', 'O2', 'H', 'H2', 'OH', 'H2O', 'HO2', 'H2O2'])[1]
+    >>> high[2][2]
+    -1.99591964e-15
+    """
+
+    db = sqlite3.connect(db_name)
+    cursor = db.cursor()
+
+    def get_species_coeffs(species_name, temp_range):
+        query = 'SELECT * FROM ' + temp_range.upper() + ' WHERE species_name = "' + species_name + '"'
+        q = cursor.execute(query).fetchall()
+        if len(q) == 0: 
+            print('Warning, did not find rows given species_name and temp_range. Please double check.')
+            return []
+        return list(q[0][3:])
+
+    low = np.zeros((len(species), 7))
+    high = np.zeros((len(species), 7))
+    for i, s in enumerate(species):
+        low[i] = get_species_coeffs(s, 'low')
+        high[i] = get_species_coeffs(s, 'high')
+    db.close()
+    return low, high
+
+
+def read_data(filename, db_name):
     """reads data from .xml reaction file
 
     INPUTS:
     =======
     filename:   str
                 File name of .xml file to parse
+    db_name:    str
+                Database name storing coefficients
 
     RETURNS:
     =======
     data: dictonary of reactions and species for each reaction in the file
-    (keys = ['reactions', 'species'])
+    (keys = ['reactions', 'species', 'low', 'high'])
 
     EXAMPLES:
     ========
-    >>> data = read_data('t.xml')
+    >>> data = read_data('t.xml', 'nasa.sqlite')
     >>> data['species']
     ['H2', 'O2', 'OH', 'HO2', 'H2O']
     """
@@ -60,6 +110,9 @@ def read_data(filename):
         phase = rxns.find('phase')
         data['species'] = phase.find('speciesArray').text.split()
 
+        # get coefficients
+        data['low'], data['high'] = get_coeffs(db_name, data['species'])
+        
         # get reaction info
         data['reactions'] = {}
         for reaction_data in rxns.findall('reactionData'):
@@ -83,8 +136,12 @@ def read_data(filename):
                                          reversible, reac_type,
                                          reac_id, coef_type, coef))
                 data['reactions'][reaction_id] = reactions
+
     except AttributeError as ex:
         print(ex)
         print('Warning: please check your xml format, ' +
               'returns empty data because format doesn\'t match')
     return data
+
+
+
