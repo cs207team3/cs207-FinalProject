02e203cff8457020f980d8d891a1149388ac22ea
zhangjams <weihangzhang@g.harvard.edu>
2017-12-05 11:38:49 -0500

    Fixed file upload messages

--- chem3/flask/f.py
+++ chem3/flask/f.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template, abort, request, redirect, url_for
+from flask import Flask, flash, render_template, abort, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
+import os
  
 app = Flask(__name__)
- 
+app.secret_key = "super secret key"
+UPLOAD_FOLDER = '/uploaded_files/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('base.html')
- 
-@app.route('/upload_file', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-        if request.files['file']:
-            f = request.files['file']
-            f.save(secure_filename(f.filename))
-            return redirect(url_for('home', messages=f.filename + ' uploaded successfully!'))
 
-@app.route('/upload_text', methods = ['GET', 'POST'])
+#-------------------------------- Upload files -------------------------------------------#
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if allowed_file(file.filename):
+            file.save(secure_filename(file.filename))
+            flash(file.filename + ' uploaded Successfully!')
+            return redirect(request.url)
+        else:
+            flash('Incorrect file format!')
+            return redirect(request.url)
+
+#-------------------------------- Get T and concs -------------------------------------------#
+@app.route('/', methods = ['GET', 'POST'])
 def upload_text():
     T = request.form['temp']
     concs = request.form['concs']
